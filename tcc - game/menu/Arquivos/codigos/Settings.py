bf577e962d540b68090348e698d2df6fcb0f4054
Alex Vieira <36942058+Allex2098@users.noreply.github.com>
2018-06-28 20:54:53 -0300

    Add files via upload

--- tcc - game/menu/Arquivos/codigos/Settings.py
+++ tcc - game/menu/Arquivos/codigos/Settings.py
@@ -8,16 +8,16 @@ def readSettings(cont):
         data = [line.split()[0] for line in reading]
         reading.close()
 
-    own["val"] = int(data[0])
-    own["bright"] = int(data[1])
+    own["bright"] = int(data[0])
+    own["val"] = int(data[1])
     own["playerName"] = data[2]
     own["audio"] = int(data[3])
 
 def writeSettings(cont):
     own = cont.owner
     with open(fileName, 'w') as writing:
-        writing.write(str(own["val"])+"\n")
         writing.write(str(own["bright"])+"\n")
+        writing.write(str(own["val"])+"\n")
         writing.write(own["playerName"]+"\n")
         writing.write(str(own["audio"]))
         
